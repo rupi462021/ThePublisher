66a3b1c0b40a81b90d704436101681f645621c23
shimi148 <Ashimi2205@gmail.com>
2021-05-24 11:51:06 +0300

    change
    asdasd

---
+++ ThePublisherM/ThePublisherM/Controllers/RestClient.cs
@@ -0,0 +1,22 @@
+﻿using RestSharp;
+using System;
+
+namespace ThePublisherM.Controllers
+{
+    internal class RestClient
+    {
+        private string v;
+
+        public RestClient(string v)
+        {
+            this.v = v;
+        }
+
+        public int Timeout { get; internal set; }
+
+        internal IRestResponse Execute(RestRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ThePublisherM/ThePublisherM/Controllers/RestRequest.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using System;
+
+namespace ThePublisherM.Controllers
+{
+    internal class RestRequest
+    {
+        private object pOST;
+
+        public RestRequest(object pOST)
+        {
+            this.pOST = pOST;
+        }
+
+        internal void AddHeader(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddParameter(string v1, string v2, ParameterType requestBody)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
