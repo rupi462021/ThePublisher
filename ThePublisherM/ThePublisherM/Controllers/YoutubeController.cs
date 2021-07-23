2903d270febb4378857f78ac1308e83b30cf0d85
Reut <80757976+rupi462021@users.noreply.github.com>
2021-07-23 11:03:07 +0300

    commit with youtube
    from 23/07/2021

---
+++ ThePublisherM/ThePublisherM/Controllers/YoutubeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ThePublisherM.Models;
+using ThePublisherM.Models.DAL;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+using ThePublisherM.utils;
+
+namespace ThePublisherM.Controllers
+{
+    public class YoutubeController : ApiController
+    {
+        // GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+        public IHttpActionResult Get(string title, string description)
+        {
+
+            UploadVideoShimi uv = new UploadVideoShimi(title, description);
+
+            uv.UploadAsync();
+
+            return Ok("Video has been uploaded");
+        }
+
+        // GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
